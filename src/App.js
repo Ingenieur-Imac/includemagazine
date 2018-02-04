1660e8d8423226231485730545f73b77c0a97e86
irene <irene.blt@free.fr>
2018-02-04 01:59:22 +0100

    css main article v1

--- src/App.js
+++ src/App.js
@@ -10,24 +10,25 @@ import Article from './components/Article'
 import MainArticle from './components/MainArticle'
 import Menu from './components/Menu'
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
-          <div>
-            <ul>
-              <li><Link to="/menu">Menu</Link></li>
-            </ul>
+        <h1 className="titleMagazine">INCLUDE</h1>
 
-            <hr/>
+        <Router>
+          <div className="containerNav">
+
+            {/*<nav>
+              <li><Link to="/menu">Menu</Link></li>
+            </nav>*/}
 
             <Route exact path="/" component={MainArticle}/>
             <Route path="/article/:id" component={Article}/>
             <Route path="/menu" component={Menu}/>
+
           </div>
         </Router>
       </div>
