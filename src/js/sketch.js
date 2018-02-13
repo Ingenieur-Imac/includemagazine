98031401fd4913df82b1c6a10fc2c31ddb737750
CoralieGold <coralie.gold@gmail.com>
2018-02-13 11:23:09 +0100

    Change serendipity grid generation

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -8,7 +8,7 @@ export default function sketch (p) {
 
     // stroke_weight = strokeWeight
 
-    var x=50, y=50, cellSz, grid=[], widthCan = 800, heightCan = 800;
+    var x=50, y=50, cellSz, grid=[], widthCan = 2000, heightCan = 2000;
     var DOWN=1, RIGHT=2, px=0, py=0, stroke_weight=2;
     var background_color = [ 255, 255, 255 ];
     var line_color = [ 0, 0, 0 ]
@@ -29,12 +29,12 @@ export default function sketch (p) {
 
         var controls = {
             divisions: x,
-            background_red : background_color[0],
+/*            background_red : background_color[0],
             background_green : background_color[1],
             background_blue : background_color[2],
             line_red : line_color[0],
             line_green : line_color[1],
-            line_blue : line_color[2],
+            line_blue : line_color[2],*/
             stroke_weight : stroke_weight
         };
         gui = new dat.gui.GUI(params);
@@ -46,12 +46,6 @@ export default function sketch (p) {
         gui.remember(controls);
         var divisions_controller = gui.add(controls, 'divisions').min(10).max(105).step(10);
         var stroke_weight_controller = gui.add(controls, 'stroke_weight').min(1).max(5).step(1);
-        var background_red_controller = gui.add(controls, 'background_red').min(0).max(255).step(10);
-        var background_green_controller = gui.add(controls, 'background_green').min(0).max(255).step(10);
-        var background_blue_controller = gui.add(controls, 'background_blue').min(0).max(255).step(10);
-        var line_red_controller = gui.add(controls, 'line_red').min(0).max(255).step(10);
-        var line_green_controller = gui.add(controls, 'line_green').min(0).max(255).step(10);
-        var line_blue_controller = gui.add(controls, 'line_blue').min(0).max(255).step(10);
 
         divisions_controller.onFinishChange(function(value) {
             x = value;
@@ -61,36 +55,6 @@ export default function sketch (p) {
 
         stroke_weight_controller.onFinishChange(function(value) {
             stroke_weight = value;
-            p.drawGrid();
-        });
-
-        background_red_controller.onFinishChange(function(value) {
-            background_color[0] = value;
-            p.drawGrid();
-        });
-
-        background_green_controller.onFinishChange(function(value) {
-            background_color[1] = value;
-            p.drawGrid();
-        });
-
-        background_blue_controller.onFinishChange(function(value) {
-            background_color[2] = value;
-            p.drawGrid();
-        }); 
-
-        line_red_controller.onFinishChange(function(value) {
-            line_color[0] = value;
-            p.drawGrid();
-        });
-
-        line_green_controller.onFinishChange(function(value) {
-            line_color[1] = value;
-            p.drawGrid();
-        });
-
-        line_blue_controller.onFinishChange(function(value) {
-            line_color[2] = value;
             p.drawGrid();
         });
     }
@@ -105,10 +69,38 @@ export default function sketch (p) {
         px = 0;
         py = 0;
 
-        p.makeGrid();
+        cellSz = p.min(p.width,p.height) / x;
 
-        p.foreach(p.drawCell);
-        p.step();
+        for (var i = 0; i < y; i++) { 
+            for (var j = 0; j < x; j++)  
+                if (i > 0 && j < y-1 && i < y-1 && i < x-1 && j < x-1) {
+       
+                var off = 0;
+                p.stroke(line_color[0], line_color[1], line_color[2]);
+                
+                if (Math.random() < 0.9) { 
+                    p.stroke(background_color[0], background_color[1], background_color[2]); // erase
+                    if (j > 1) off = stroke_weight;
+                }
+
+                if (j  > 0) {
+
+                    p.line(j * cellSz + off, cellSz + i * cellSz, // horiz 
+                            (j + 1) * cellSz-1, cellSz + i * cellSz);
+                } 
+
+                off = 0;
+                p.stroke(line_color[0], line_color[1], line_color[2]);
+                
+                if (Math.random() > 0.9) {
+                    p.stroke(background_color[0], background_color[1], background_color[2]); // erase
+                    if (i > 1) off = stroke_weight;
+                }
+
+                p.line((cellSz + j * cellSz), i * cellSz + off,
+                        (cellSz + j * cellSz), (i + 1) * cellSz);   
+            }
+        }
     }
 
     p.setup = function () {
@@ -141,90 +133,5 @@ export default function sketch (p) {
             p.initGui();
             p.drawGrid();
         }, false);
-    }
-
-    p.drawCell = function (j,i) {
-
-        // skip one row/col around edge
-        if (i > 0 && j < y-1 && i < y-1 && i < x-1 && j < x-1) {
-       
-            var off = 0;
-            p.stroke(line_color[0], line_color[1], line_color[2]);
-            if (!p.exists(j, i, DOWN)) { 
-                p.stroke(background_color[0], background_color[1], background_color[2]); // erase
-                if (j > 1) off = stroke_weight;
-            }
-
-            if (j  > 0) {
-
-                p.line(j * cellSz + off, cellSz + i * cellSz, // horiz 
-                        (j + 1) * cellSz-1, cellSz + i * cellSz);
-            } 
-
-            off = 0;
-            p.stroke(line_color[0], line_color[1], line_color[2]);
-            if (!p.exists(j, i, RIGHT)) {
-                p.stroke(background_color[0], background_color[1], background_color[2]); // erase
-                if (i > 1) off = stroke_weight;
-            }
-
-            p.line((cellSz + j * cellSz), i * cellSz + off, // vert 
-                    (cellSz + j * cellSz), (i + 1) * cellSz);   
-        }
-    }
-
-    p.makeGrid = function () {
-
-        cellSz = p.min(p.width,p.height) / x;
-
-        for (var j = 0; j < x+1; j++) { 
-
-            grid[j] = []; // initialize to 0 
-            for (var i = 0; i < y+1; i++) 
-                grid[j][i] = 0;
-        }
-    }
-
-    p.step = function () {
-
-        var dirs = (Math.random() < .9)  ? [DOWN] : [RIGHT,DOWN];
-        var idx = p.floor(p.random(dirs.length));
-        var dir = dirs[idx];
-        p.remove_sketch(px, py, dir);
-
-        // but weight verticals and the diagonal
-        var d = p.dist(px,0,py,0) / x;
-        if (Math.random()  < d) 
-            p.remove_sketch(px, py, RIGHT);
-
-        p.drawCell(px,py); 
-
-        if (++px % x === 0) { px = 0; ++py; } 
-
-        if (py < y-1 || px < x-1) 
-            p.step();
-    }
-
-    p.foreach = function (fun) {
-
-        for (var i = 0; i < y; i++) { 
-            for (var j = 0; j < x; j++)  
-                fun.apply(this,[j,i]);
-        }   
-    }
-
-    p.exists = function (j,i,side) {
-
-        switch(side) {
-            case RIGHT: return (grid[j][i] < 2);
-            case DOWN:  return (grid[j][i] % 2===0);
-            default: return (grid[j][i] < 2);
-        }
-    }
-
-    p.remove_sketch = function (j,i,side) {
-
-        if (p.exists(j,i,side)) 
-            grid[j][i] += side;
     }
 };
